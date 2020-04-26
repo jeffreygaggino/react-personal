e9c06b0ac87124f503fceeed2df8c57d969cecb1
Jeffrey Gaggino <jeffrey@hismileteeth.com>
2020-04-26 17:08:52 +1000

    [u] header

--- src/components/burger/burger.jsx
+++ src/components/burger/burger.jsx
@@ -2,18 +2,12 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class Burger extends Component {
-  state = {
-    open: false,
-  };
-  burgerToggle = () => {
-    this.setState({ open: !this.state.open });
-  };
   render() {
     return (
       <React.Fragment>
         <div
-          onClick={this.burgerToggle}
-          className={`burger${this.state.open ? " open" : ""}`}
+          onClick={this.props.burgerToggle}
+          className={`burger${this.props.open ? " open" : ""}`}
         >
           <div className="burger__line-1 burger__child"></div>
           <div className="burger__line-2 burger__child"></div>
@@ -23,15 +17,23 @@ class Burger extends Component {
           </div>
           <div className="burger__line-5 burger__child"></div>
         </div>
-        <div className={`burger-overlay${this.state.open ? " open" : ""}`}>
+        <div className={`burger-overlay${this.props.open ? " open" : ""}`}>
           <div className="burger-overlay__nav">
-            <Link onClick={this.burgerToggle} to="/" className="nav-item">
+            <Link onClick={this.props.burgerToggle} to="/" className="nav-item">
               Home
             </Link>
-            <Link onClick={this.burgerToggle} to="/about" className="nav-item">
+            <Link
+              onClick={this.props.burgerToggle}
+              to="/about"
+              className="nav-item"
+            >
               Who Am I?
             </Link>
-            <Link onClick={this.burgerToggle} to="/skills" className="nav-item">
+            <Link
+              onClick={this.props.burgerToggle}
+              to="/skills"
+              className="nav-item"
+            >
               Expertise
             </Link>
             <a
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,16 +4,25 @@ import ModeSwitch from "../modeswitch/modeswitch";
 import Burger from "../burger/burger";
 
 class header extends Component {
+  state = {
+    open: false,
+  };
+  burgerToggle = () => {
+    this.setState({ open: !this.state.open });
+  };
+  wordToggle = () => {
+    this.setState({ open: false });
+  };
   render() {
     return (
       <header className="headerNav">
         <ModeSwitch />
-        <div className="headerNav__wordmark">
+        <div onClick={this.wordToggle} className="headerNav__wordmark">
           <Link to="/">
             J<span className="punctuation">.</span>Gaggino
           </Link>
         </div>
-        <Burger />
+        <Burger open={this.state.open} burgerToggle={this.burgerToggle} />
       </header>
     );
   }
